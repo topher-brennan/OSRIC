799f3c1035c185928e49b238e2c638aa3b14e86c
Topher Brennan <topher@tophers-air.attlocal.net>
2020-04-16 16:44:13 -0700

    Various changes
    * Give monsters armor classes and ability to calculate to-hit numbers
    * Create module with helper method to roll string representations of dice

--- monster.rb
+++ monster.rb
@@ -11,53 +11,93 @@ class Monster
   def hit_dice
     raise NotImplementedError
   end
+
+  # TODO: Handle monsters with HD less than 1-1
+  def equivalent_level
+    if hit_dice.last < 0
+      return 1
+    else
+      return hit_dice.first + 1 + (hit_dice.last > 0 ? 1 : 0)
+    end
+  end
+
+  def to_hit(armor_class)
+    result = 21 - armor_class - [equivqlent_level, 20].min
+    result -= 5 if result > 20
+    result
+  end
+
+  def armor_class
+    self::ARMOR_CLASS
+  end
 end
 
 # Listed in rough order of power level, which will be used in my first pass simulation of raising a PC from level 1 to level 9.
 
 class Kobold < Monster
+  ARMOR_CLASS = 7
+
+  # TODO: This entire implementation is hacky, if I had a lot of sub 1-1 HD monsters I should write up a more general solution.
   def roll_hp
     rand(4)+1
+  end
+
+  def equivalent_level
+    0
   end
 end
 
 class Goblin < Monster
+  ARMOR_CLASS = 6
+
   def hit_dice
     [1, -1]
   end
 end
 
 class Orc < Monster
+  ARMOR_CLASS = 6
+
   def hit_dice
     [1, 0]
   end
 end
 
 class Hobgoblin < Monster
+  ARMOR_CLASS = 5
+
   def hit_dice
     [1, 1]
   end
 end
 
-class LizardMan < Monster
+class Gnoll < Monster
+  ARMOR_CLASS = 5
+
   def hit_dice
-    [2, 1]
+    [2, 0]
   end
 end
 
 class Bugbear < Monster
+  ARMOR_CLASS = 5
+
   def hit_dice
     [3, 1]
   end
 end
 
 class Ogre < Monster
+  ARMOR_CLASS = 5
+
   def hit_dice
     [4, 1]
   end
 end
 
 class Owlbear < Monster
+  ARMOR_CLASS = 6
+
   def hit_dice
     [5, 1]
   end
---
+++ osric_support.rb
@@ -0,0 +1,31 @@
+module OsricSupport
+  def self.roll(str)
+    parsing = []
+    number_builder = ""
+    
+    str.each_char do |char|
+      if char.match?(/[0-9]/)
+        number_builder << char
+      elsif char.match?(/[d+-]/)
+        parsing << number_builder.to_i
+        number_builder = ""
+        parsing << char
+      else
+        raise ArgumentError
+      end
+    end
+
+    parsing << number_builder.to_i if !number_builder.empty?
+
+    parsing = parsing.take(1) + ['d', 8] + parsing.drop(1) if parsing[1] != 'd'
+
+    result = (1..parsing[0]).map { rand(parsing[2]) + 1 }.inject(:+)
+    
+    if parsing[3] == '+'
+      result += parsing[4]
+    elsif parsing[3] == '-'
+      result -= parsing[4]
+    end
+    result
+  end
+end
